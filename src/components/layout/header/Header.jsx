2922c8fdf52cccf58558cf86a985a88432f90da4
Pierre Schnell <pierreschnell@hotmail.com>
2024-06-17 13:50:37 +0200

    modification cv

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
                   <FontAwesomeIcon icon={faLinkedinIn} />
                </a>
                <a
-                  href="https://drive.google.com/file/d/15J6OQzU_ldiSXZ2KY9yp89803uqjcfVQ/view?usp=drive_link"
+                  href="https://drive.google.com/file/d/12lqHOMwE_2pubwhBMyNNWX4zlfw3m_7Z/view?usp=drive_link"
                   target="_blank"
                   rel="noopener noreferrer"
                   title="CV"
